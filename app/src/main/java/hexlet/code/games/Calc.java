5c29cdd0af8c63ce60c4f38d1a92d575542da1e0
sodomova_k <sodomova.ekaterina@yandex.ru>
2022-10-15 11:00:42 +0300

    update Calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,43 +1,78 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
 import hexlet.code.Util;
 
 public class Calc {
+    final static String RULE_OF_GAME = "What is the result of the expression?";
     private static String question;
     private static int correctAnswer;
 
-    public static String getGameCondition() {
-        return "What is the result of the expression?";
+    public static void startPlaying() {
+        Engine.playGame("Calc");
     }
 
-    public static String getQuestion() {
+    public static String getRuleOfGame() {
+        return RULE_OF_GAME;
+    }
+
+    private static String getQuestion() {
         return question;
     }
 
-    public static String getCorrectAnswer() {
+    private static String getCorrectAnswer() {
         return Integer.toString(correctAnswer);
     }
 
-    public static void setResult() {
+    private static void setResultGame() {
         int firstNumber = Util.randomNumber();
         int secondNumber = Util.randomNumber();
-        int arithmeticOperation = Util.randomNumber(1, 3);
+        String arithmeticOperation = Integer.toString(Util.randomNumber(1, 3));
+        char arithmeticOperationSymbol;
 
         switch (arithmeticOperation) {
-            case 1 :
-                correctAnswer = firstNumber + secondNumber;
-                question = firstNumber + " + " + secondNumber;
+            case "1" :
+                arithmeticOperationSymbol = '+';
                 break;
-            case 2 :
-                correctAnswer = firstNumber - secondNumber;
-                question = firstNumber + " - " + secondNumber;
+            case "2" :
+                arithmeticOperationSymbol = '-';
                 break;
-            case 3 :
-                correctAnswer = firstNumber * secondNumber;
-                question = firstNumber + " * " + secondNumber;
+            case "3" :
+                arithmeticOperationSymbol = '*';
                 break;
             default:
-                break;
+                throw new RuntimeException("The value of " + arithmeticOperation + " does not match the list of possible values. Possible values: 1, 2, 3.");
         }
+
+        calculate(firstNumber, secondNumber, arithmeticOperationSymbol);
+    }
+
+    private static void calculate(int firstNumber, int secondNumber, char arithmeticOperationSymbol) {
+        switch (arithmeticOperationSymbol) {
+            case '+' :
+                question = firstNumber + " + " + secondNumber;
+                correctAnswer = firstNumber + secondNumber;
+                break;
+            case '-' :
+                question = firstNumber + " - " + secondNumber;
+                correctAnswer = firstNumber - secondNumber;
+                break;
+            case '*' :
+                question = firstNumber + " * " + secondNumber;
+                correctAnswer = firstNumber * secondNumber;
+                break;
+            default:
+                throw new RuntimeException("Unknown operator" + arithmeticOperationSymbol);
+        }
+    }
+
+    public static String[] getResultGame() {
+        String[] resultGame = new String[2];
+
+        setResultGame();
+        resultGame[0] = getQuestion();
+        resultGame[1] = getCorrectAnswer();
+
+        return resultGame;
     }
 }
