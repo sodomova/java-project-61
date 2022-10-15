613921263e626d3b3abc47fecd1e85521e5c5740
sodomova_k <sodomova.ekaterina@yandex.ru>
2022-10-15 13:09:15 +0300

    update GCD

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,31 +1,46 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
 import hexlet.code.Util;
 
 public class GCD {
+    public static final String RULE_OF_GAME = "Find the greatest common divisor of given numbers.";
     private static String question;
     private static int correctAnswer;
 
-    public static String getGameCondition() {
-        return "Find the greatest common divisor of given numbers.";
+    public static void startPlaying() {
+        Engine.playGame("GCD");
     }
 
-    public static String getQuestion() {
+    private static String getQuestion() {
         return question;
     }
 
-    public static String getCorrectAnswer() {
+    private static String getCorrectAnswer() {
         return Integer.toString(correctAnswer);
     }
 
-    public static void setResult() {
+    public static String[] getResultGame() {
+        String[] resultGame = new String[2];
+
+        setResultGame();
+
+        resultGame[0] = getQuestion();
+        resultGame[1] = getCorrectAnswer();
+
+        return resultGame;
+    }
+
+    private static void setResultGame() {
         int firstNumber = Util.randomNumber();
         int secondNumber = Util.randomNumber();
+
         question = firstNumber + " " + secondNumber;
+
         setCorrectAnswer(firstNumber, secondNumber);
     }
 
-    public static void setCorrectAnswer(int firstNumber, int secondNumber) {
+    private static void setCorrectAnswer(int firstNumber, int secondNumber) {
         while (firstNumber != secondNumber) {
             if (firstNumber > secondNumber) {
                 firstNumber = firstNumber - secondNumber;
@@ -33,6 +48,7 @@ public class GCD {
                 secondNumber = secondNumber - firstNumber;
             }
         }
+
         correctAnswer = firstNumber;
     }
 }
