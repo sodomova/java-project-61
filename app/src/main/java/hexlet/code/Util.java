a34c1c8eba83763221d9844fe77437da5f5e1461
sodomova_k <sodomova.ekaterina@yandex.ru>
2022-10-15 13:11:11 +0300

    update Util

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -1,11 +1,10 @@
 package hexlet.code;
 
 public class Util {
+    static final int MIN = 1;
+    static final int MAX = 100;
 
     public static int randomNumber() {
-        final int MIN = 1;
-        final int MAX = 100;
-
         int randomNumber = (int) (Math.random() * (MAX - MIN)) + MIN;
 
         return randomNumber;
