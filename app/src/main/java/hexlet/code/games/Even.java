cedd6f42d593594009d555671e13a284fa524bb6
sodomova_k <sodomova.ekaterina@yandex.ru>
2022-10-18 21:54:45 +0300

    update Even

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -4,17 +4,23 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Even {
-    public static final String RULE_OF_GAME = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    private static final String RULE_OF_GAME = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
     public static void startPlaying() {
-        Engine.playGame("Even");
+        String[][] gameData = new String[Engine.MOVES][2];
+
+        for (int i = 0; i < Engine.MOVES; i++) {
+            gameData[i] = getResultGame();
+        }
+
+        Engine.playGame(RULE_OF_GAME, gameData);
     }
 
     private static boolean isEven(int question) {
         return question % 2 == 0;
     }
 
-    public static String[] getResultGame() {
+    private static String[] getResultGame() {
         int number = Utils.randomNumber();
 
         String question = Integer.toString(number);
