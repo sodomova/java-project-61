49619ab150c4b17cc5ffabc4398584e36e95d173
sodomova_k <sodomova.ekaterina@yandex.ru>
2022-10-18 21:53:58 +0300

    update Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,24 +1,17 @@
 package hexlet.code;
 
-import hexlet.code.games.Calc;
-import hexlet.code.games.Even;
-import hexlet.code.games.GCD;
-import hexlet.code.games.Prime;
-import hexlet.code.games.Progression;
-
 import java.util.Scanner;
+
 public class Engine {
-    static final int MOVES = 3;
-    private static String userName;
+    public static final int MOVES = 3;
 
-    public static void playGame(String nameGame) {
-        greet();
-        getRuleOfGame(nameGame);
+    public static void playGame(String ruleOfGame, String[][] gameData) {
+        String userName = greet();
+        System.out.println(ruleOfGame);
 
-        for (int i = 0; i < MOVES; i++) {
-            String[] resultsGame = getResultGame(nameGame);
-            String question = resultsGame[0];
-            String correctAnswer = resultsGame[1];
+        for (String[] resultGame : gameData) {
+            String question = resultGame[0];
+            String correctAnswer = resultGame[1];
 
             System.out.print("Question: " + question);
             System.out.print("\nYour answer: ");
@@ -39,61 +32,14 @@ public class Engine {
         System.out.println("Congratulations, " + userName + "!");
     }
 
-    private static void greet() {
+    private static String greet() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        userName = scanner.nextLine();
+        String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
-    }
 
-    private static void getRuleOfGame(String nameGame) {
-        switch (nameGame) {
-            case "Even" :
-                System.out.println(Even.RULE_OF_GAME);
-                break;
-            case "Calc" :
-                System.out.println(Calc.RULE_OF_GAME);
-                break;
-            case "GCD" :
-                System.out.println(GCD.RULE_OF_GAME);
-                break;
-            case "Progression" :
-                System.out.println(Progression.RULE_OF_GAME);
-                break;
-            case "Prime" :
-                System.out.println(Prime.RULE_OF_GAME
-                );
-                break;
-            default:
-                throw new RuntimeException("The game '" + nameGame + "' doesn't exist.");
-        }
-    }
-
-    private static String[] getResultGame(String nameGame) {
-        String[] resultGame = new String[2];
-
-        switch (nameGame) {
-            case "Even" :
-                resultGame = Even.getResultGame();
-                break;
-            case "Calc":
-                resultGame = Calc.getResultGame();
-                break;
-            case "GCD":
-                resultGame = GCD.getResultGame();
-                break;
-            case "Progression":
-                resultGame = Progression.getResultGame();
-                break;
-            case "Prime":
-                resultGame = Prime.getResultGame();
-                break;
-            default:
-                throw new RuntimeException("The game '" + nameGame + "' doesn't exist.");
-        }
-
-        return resultGame;
+        return userName;
     }
 }
